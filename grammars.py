1e7b776369b85d42bf92a2bcfd55bffe1b2c88a1
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-05-25 00:04:29 -0700

    Switch to terms.

--- grammars.py
+++ grammars.py
@@ -2,24 +2,23 @@ from parsley import makeGrammar
 from terml.nodes import termMaker as t
 
 g = """
-crlf = '\r' '\n' -> "<br />"
-doublecrlf = crlf crlf -> "</p><p>"
+crlf = '\r' '\n' -> t.Break()
+doublecrlf = crlf crlf -> t.DoubleBreak()
 not_crlf = ~crlf anything
 crlfs = doublecrlf | crlf
 
 single_star = '*' ~'*'
 double_star = '*' '*'
 
-bold = double_star (~double_star nested_decos)+:b double_star
-    -> "<b>%s</b>" % "".join(b)
+bold = double_star (~double_star nested_decos)+:b double_star -> t.Bold(b)
 
 italics = single_star (~single_star nested_decos)+:i single_star
-    -> "<i>%s</i>" % "".join(i)
+       -> t.Italics(i)
 
-underline = '_' (~'_' nested_decos)+:u '_' -> "<u>%s</u>" % "".join(u)
+underline = '_' (~'_' nested_decos)+:u '_' -> t.Underline(u)
 
 greentext = crlfs:head '>' nested_decos+:body crlfs:tail
-         -> '%s<span class="quote">&gt;%s</span>%s' % (head, "".join(body), tail)
+         -> t.Greentext(head, body, tail)
 
 decorations = bold | italics | underline
 
@@ -27,7 +26,7 @@ nested_decos = decorations | not_crlf
 
 entities = greentext | crlfs | decorations
 
-paragraphs = (entities | anything)*:l -> "<p>%s</p>" % "".join(l)
+paragraphs = (entities | anything)*:l -> t.Paragraphs(l)
 """
 
 grammar = makeGrammar(g, {"t": t})
